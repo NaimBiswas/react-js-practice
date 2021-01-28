4c402a52925847e9fbcc15208dd3c35b307fdb87
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-28 12:45:54 +0600

    run my first code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Hello <code>src/App.js</code> World.
+         Hello World.
         </p>
         <a
           className="App-link"
