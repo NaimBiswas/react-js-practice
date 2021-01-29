20c4bfb3e7b406aa359a6ff2d1debec579ebc51c
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-29 21:51:48 +0600

    set a constructor condition

--- src/components/ConditionOne.js
+++ src/components/ConditionOne.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export default class ConditionOne extends Component {
+   constructor () {
+      super()
+      this.state ={
+         Login: true
+      }
+   }
    render() {
       return (
          <div>
