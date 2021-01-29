6dfb50c0beb6426a19b86e47c9264084da5459ce
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-29 20:55:18 +0600

    create my own

--- src/components/State.js
+++ src/components/State.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react'
 
-export default class State extends Component {
+export default class State extends Component{
    render() {
       return (
          <div>
             
          </div>
       )
-   }
+   };
 }
