2f833d4daed98f3953d5b63779e2ded335ef7feb
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-29 12:43:48 +0600

    import bootstrap globaly

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.svg';
 import './App.css';
 import First from './components/First';
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import  '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
 function Welcome()  {
    return (
       <div>
          
-         <p className="mt-4 text-danger">This is BootStrap Butoon Test</p>
-        <button className='btn btn-info btn-lg mt-1 mb-4'>Test Bootstrap Button</button> 
+         <div className="container">
+            <p className="mt-4 text-danger">This is BootStrap Butoon Test</p>
+            <button className='btn btn-info btn-lg mt-1 mb-4'>Test Bootstrap Button</button> 
+         </div>
    
       </div>
    );
