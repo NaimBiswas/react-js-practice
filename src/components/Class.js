cf0556caa160e6bd40c8485e19d1c375d4dd04ae
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-29 12:01:26 +0600

    class based props test success

--- src/components/Class.js
+++ src/components/Class.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 
 class FromClass extends Component{
    render() {
-      return <h1>THis is test class function </h1>
+      return <h1>THis is test class function and I am {this.props.name }</h1>
    }
 }
 export default FromClass;
