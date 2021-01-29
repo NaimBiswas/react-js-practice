a648f560dbffe69ba987f0512cd71079983d2f73
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-29 22:59:34 +0600

    set formate

--- src/App.js
+++ src/App.js
@@ -1,73 +1,65 @@
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import logo from './logo.svg';
-import './App.css';
-import First from './components/First';
-import Class from './components/Class';
-import ButtonFun from './components/ButtonFun';
-import ButtonClass from './components/ButtonClass';
-import Welcome from './components/Welcome';
-import { ArrowFunction } from './components/ArrowFunction';
-import State from './components/State';
-import StateChange from './components/StateChange';
-import ConditionOne from './components/ConditionOne';
-import Refesh from './components/Refesh';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import logo from "./logo.svg";
+import "./App.css";
+import First from "./components/First";
+import Class from "./components/Class";
+import ButtonFun from "./components/ButtonFun";
+import ButtonClass from "./components/ButtonClass";
+import Welcome from "./components/Welcome";
+import { ArrowFunction } from "./components/ArrowFunction";
+import State from "./components/State";
+import StateChange from "./components/StateChange";
+import ConditionOne from "./components/ConditionOne";
+import Refesh from "./components/Refesh";
 
 function HOp() {
    return (
       <div className="App">
+         
          {/* ForceUpdate  */}
          <Refesh></Refesh>
          {/* Login LogOut Condition  */}
-         <ConditionOne></ConditionOne>
-         {/* State Change  */}
-         <StateChange></StateChange>
-         {/* State Component  */}
-         <State></State>
-         {/* this is arrow Fucntin Test  */}
-         <ArrowFunction></ArrowFunction>
-         {/* Bootstrap Button  */}
-         <Welcome></Welcome>
- 
-         {/* Class Based Button  */}
-         <ButtonClass></ButtonClass>
-            
-
-         {/* Function Based On click button  */}
-         <ButtonFun></ButtonFun>
-         
-         
-        {/* class based funtin  */}
-         <Class   name="Naim Biswas"  ></Class>
-         <Class  name="Naim Biswas 1254"  ></Class>
-         <Class  name="Naim Biswas 1254"  ></Class>
-         <Class  name="Naim Biswas 1254"  ></Class>
-         
-         
+         <ConditionOne> </ConditionOne> {/* State Change  */}
+         <StateChange> </StateChange> {/* State Component  */} <State> </State>
+         {/* this is arrow Fucntin Test  */} <ArrowFunction></ArrowFunction>
+         {/* Bootstrap Button  */} <Welcome></Welcome>
+         {/* Class Based Button  */} <ButtonClass> </ButtonClass>
+         {/* Function Based On click button  */} <ButtonFun></ButtonFun>
+         {/* class based funtin  */} <Class name="Naim Biswas"> </Class>
+         <Class name="Naim Biswas 1254"> </Class>
+         <Class name="Naim Biswas 1254"> </Class>
+         <Class name="Naim Biswas 1254"> </Class>
          {/* function based function  */}
-        <div>
-           <h2 className=''>This is Naim Biswas</h2>
-           <First name="Naim"   age="20"></First>
-           <First name="Biswas" age="20"></First>
-           <First name="Alamin" age="20"></First>
-           <First  name="Tuttul" age="20"></First>
+         <div>
+            <h2 className=""> This is Naim Biswas </h2>
+            <First name="Naim" age="20">
+               
+            </First>
+            <First name="Biswas" age="20">
+               
+            </First>
+            <First name="Alamin" age="20">
+               
+            </First>
+            <First name="Tuttul" age="20">
+               
+            </First>
          </div>
          {/* defualt function  */}
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className='TryMe'>
-         Hello World.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        </header>
-    </div>
-  );
+         <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p className="TryMe">Hello World. </p>
+            <a
+               className="App-link"
+               href="https://reactjs.org"
+               target="_blank"
+               rel="noopener noreferrer"
+            >
+               Learn React
+            </a>
+         </header>
+      </div>
+   );
 }
 
 export default HOp;
