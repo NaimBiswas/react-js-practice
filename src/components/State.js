c1d2eda27d91d72db0da977edcfe32fcdff74a06
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-29 21:07:39 +0600

    successfully showing all state

--- src/components/State.js
+++ src/components/State.js
@@ -7,6 +7,8 @@ export default class State extends Component{
       this.state = {
          name: 'Naim Biswas',
          age: 12,
+         Height: '5.67"',
+         Width: '80kg',
      } 
    }
    render() {
@@ -14,7 +16,13 @@ export default class State extends Component{
          <div>
             <h3>{this.state.name}</h3>
             <h3 className="text-info">{this.state.age}</h3>
+            <h3 className="text-success">{this.state.Height}</h3>
+            <h3 className="text-danger">{this.state.Width}</h3>
+            <div>
+            
+            </div>
          </div>
+         
       )
    };
 }
