36e4adde82b7471b6a74df5cad91b2d0fa623b15
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-29 21:34:01 +0600

    successfully change state by click

--- src/components/StateChange.js
+++ src/components/StateChange.js
@@ -4,14 +4,20 @@ export default class StateChange extends Component {
    constructor () {
       super()
       this.state = {
-         
+         name: 'Default Name',
       }
-   }
+   };
+   
+    ChangeName(a) {
+     this.setState({ name: a }) 
+   };
    render() {
       return (
          <div>
-            
+            <p>SetSate Name: {this.state.name}</p>
+            <button onClick={ this.ChangeName.bind(this, 'ChangedName') }>Click Here to Change State</button>
          </div>
       )
-   }
+   };
+   
 }
