d6ce445508af85d8dd2d298e00911ccbdd65bab4
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-29 12:10:47 +0600

    comments

--- src/components/ButtonFun.js
+++ src/components/ButtonFun.js
@@ -1,11 +1,13 @@
 import React from 'react'
 function ButtonFun() {
-   function  DoThis() {
-      alert('This button has been clided');
-   };
+  
    return (
       <button onClick={DoThis} >This is button</button>
-   )
+   );
+   // Button Click Fucntion 
+    function  DoThis() {
+      alert('This button has been clided');
+   };
 }
 
 export default ButtonFun;
