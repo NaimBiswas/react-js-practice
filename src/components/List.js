e7176995535fa3423164fa34381318cd910e2272
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-30 20:51:36 +0600

    showing list form json

--- src/components/List.js
+++ src/components/List.js
@@ -2,9 +2,15 @@ import React, { Component } from 'react'
 
 export default class List extends Component {
    render() {
+      const country = ['Bangledesh', 'India', 'Canada', 'Bahrain', 'USA', 'UK']
+     const dataItems =  country.map((AllCountry)=>{
+         return  <li>{AllCountry}</li>
+      })
       return (
          <div>
-            
+            <ol>
+               {dataItems}
+            </ol>
          </div>
       )
    }
