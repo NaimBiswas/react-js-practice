2984dde07b8de2ad6dafd11a7582f09c442cba81
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-30 10:30:01 +0600

    create a fucntion

--- src/components/Reactdom.js
+++ src/components/Reactdom.js
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import ReactDOM from 'react-dom'
 export const Reactdom = () => {
+   
    return (
       <div>
-         
+         <button onClick={Myfun}>Change by click</button>
+         <h3>This is React DOM Change </h3>
       </div>
    )
+   function Myfun() {
+      
+   }
 }
