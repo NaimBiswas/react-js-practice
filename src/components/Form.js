9ef7edefafcc625df4909d3706bc76139b2d9b6a
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-30 11:41:41 +0600

    create a single from

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+   return (
+      <div>
+         <h4>This is Form </h4>
+         <Form actions='' >
+            <input type="text" placeholder='Enter Your Text Here....'></input>
+            <input type='text' value='Submit Now'></input>
+         </Form>
+      </div>
+   )
+}
+
+export default Form
