82cd1967faac70091845c8fa8e9a8c534e5c7d02
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-30 13:07:54 +0600

    create a basic register form

--- src/components/SingUpForm.js
+++ src/components/SingUpForm.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react'
 
 export default class SingUpForm extends Component {
-   
+   constructor () {
+      super()
+      this.state = {
+         fname: '',
+         lname: '',
+         email: '',
+         phone: '',
+      }
+   }
    
    
    render() {
       return (
-         <div>
-            
+         <div className='col-lg-12'>
+            <form>
+               <input type="text" placeholder='Enter Your Frist Name' name='fname' className='form-control'></input> <br></br>
+               
+               <input type="text" placeholder='Enter Your Last Name' name='lname' className='form-control'></input> <br></br>
+               
+               <input type="email" placeholder='Enter Your Email Name' name='email' className='form-control'></input> <br></br>
+               
+               <input type="number" placeholder='Enter Your Phone Name' name='phone' className='form-control'></input> <br></br>
+               
+               <input type='submit' value='Submit'></input>
+               <br></br>
+               <br></br>
+               <hr></hr>
+            </form>
          </div>
       )
    }
