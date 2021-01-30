196bdd94ba1df62ba22a37479b8c15abefad32db
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-30 20:54:55 +0600

    select option added

--- src/components/List.js
+++ src/components/List.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react'
 
 export default class List extends Component {
+   
    render() {
       const country = ['Bangledesh', 'India', 'Canada', 'Bahrain', 'USA', 'UK']
      const dataItems =  country.map((AllCountry)=>{
-         return  <li>{AllCountry}</li>
+         return  <option>{AllCountry}</option>
       })
       return (
          <div>
-            <ol>
+            <select>
                {dataItems}
-            </ol>
+            </select>
          </div>
       )
    }
