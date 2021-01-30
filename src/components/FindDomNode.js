1525175cce468f562479d260953cbcc3e54477df
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-30 11:34:45 +0600

    successfully worked FindDomNodel

--- src/components/FindDomNode.js
+++ src/components/FindDomNode.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import Logo from '../logo.svg'
+import ReactDOM from 'react-dom'
 
 const FindDomNode = () => {
    return (
       <div>
-         <button> Change logo</button>
-         <img className='col-sm-2'  src={Logo} alt='Logo'></img>
+         <button onClick={ChangeImg}> Change logo</button>
+         <img id='Images' className='col-sm-2'  src={Logo} alt='Logo'></img>
          <h3>Find DOM Node</h3>
       </div>
    )
+   function ChangeImg() {
+      var component = document.getElementById('Images');
+      ReactDOM.findDOMNode(component).style.background='red';
+      
+   }
 }
 
 export default FindDomNode
