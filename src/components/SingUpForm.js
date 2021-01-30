5edbad37be625fa29ad2413388fe0a75c59ec173
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-30 13:10:28 +0600

    set onChange handelar

--- src/components/SingUpForm.js
+++ src/components/SingUpForm.js
@@ -10,19 +10,22 @@ export default class SingUpForm extends Component {
          phone: '',
       }
    }
+   onChange = () => {
+      
+   }
    
    
    render() {
       return (
          <div className='col-lg-12'>
             <form>
-               <input type="text" placeholder='Enter Your Frist Name' name='fname' className='form-control'></input> <br></br>
+               <input onChange={this.onChange} type="text" placeholder='Enter Your Frist Name' name='fname' className='form-control'></input> <br></br>
                
-               <input type="text" placeholder='Enter Your Last Name' name='lname' className='form-control'></input> <br></br>
+               <input onChange={this.onChange} type="text" placeholder='Enter Your Last Name' name='lname' className='form-control'></input> <br></br>
                
-               <input type="email" placeholder='Enter Your Email Name' name='email' className='form-control'></input> <br></br>
+               <input onChange={this.onChange} type="email" placeholder='Enter Your Email Name' name='email' className='form-control'></input> <br></br>
                
-               <input type="number" placeholder='Enter Your Phone Name' name='phone' className='form-control'></input> <br></br>
+               <input onChange={this.onChange} type="number" placeholder='Enter Your Phone Name' name='phone' className='form-control'></input> <br></br>
                
                <input type='submit' value='Submit'></input>
                <br></br>
