b3b6d55b24b72af41511a807dcc091f09b155e71
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-30 12:53:32 +0600

    successfully worked multipul from setState

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,13 +4,25 @@ export default class Form extends Component {
    constructor () {
       super()
       this.state = {
-         name: "THis is test name"
+         FirstName: "",
+         MiddleName: "",
+         LastName: ""
       }
    }
    
     onCahngeHandelar=(event)=>{
-       var NewName = event.target.value;
-      this.setState({name: NewName})
+       var FirstName = event.target.name;
+      var NewFristName = event.target.value;       
+       this.setState({ [FirstName]: NewFristName })
+       
+       var MiddleName = event.target.name;
+      var NewMiddleName = event.target.value;       
+       this.setState({ [MiddleName]: NewMiddleName })
+       
+       var LastName = event.target.name;
+      var NewLastName = event.target.value;       
+      this.setState({[LastName]: NewLastName})
+      
     }
    
    
@@ -19,12 +31,20 @@ export default class Form extends Component {
       <div className='col-lg-12'>
          <h4>This is Form </h4>
          <form >
-            <input onChange={this.onCahngeHandelar} type="text" className='form-control' placeholder='Enter Your Text Here....'></input> <br></br>
+               <input name='FirstName' onChange={this.onCahngeHandelar} type="text" className='form-control' placeholder='Enter Your Text Here....'></input> <br></br>
+               
+               <input name='MiddleName' onChange={this.onCahngeHandelar} type="text" className='form-control' placeholder='Enter Your Text Here....'></input> <br></br>
+               
+               <input name='LastName' onChange={this.onCahngeHandelar} type="text" className='form-control' placeholder='Enter Your Text Here....'></input> <br></br>
+               
+               
             <input type='submit' className='form-control btn-outline-danger' value='Submit Now'></input> <br></br>
          </form>
          <div>
             <h2>Your Input Below</h2>
-               <h1 className="text-danger">This is Your Text {this.state.name}</h1>
+               <h6 className="text-danger">This is Your Text : {this.state.FirstName}</h6>
+               <h6 className="text-danger">This is Your Text :  {this.state.MiddleName}</h6>
+               <h6 className="text-danger">This is Your Text : {this.state.LastName}</h6>
                <hr></hr>
          </div>
       </div>
