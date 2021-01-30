7f14ded9e29e9cca5604eb5bd0a867b97c3276bc
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-30 20:41:04 +0600

    set some class

--- src/components/Select.js
+++ src/components/Select.js
@@ -13,10 +13,10 @@ export default class Select extends Component {
    }
    render() {
       return (
-         <div>
+         <div className='mb-4 container'>
             <h4>This is your selected Area</h4>
             <h6 className="text-danger">{ this.state.name}</h6>
-            <select onChange={this.onChangeHandeler} value={this.state.name}>
+            <select className='form-control' onChange={this.onChangeHandeler} value={this.state.name}>
                <option>Dhaka </option>
                <option>Rajshahi</option>
                <option>Pabna</option>
