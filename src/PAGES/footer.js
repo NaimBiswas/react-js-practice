9f8a76e791c31f26153ae76479b6ddcc19b73a29
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-31 22:31:59 +0600

    set conatiner and margin

--- src/PAGES/footer.js
+++ src/PAGES/footer.js
@@ -5,7 +5,7 @@ export class Footer extends Component {
       return (
          <div  className='text-center ' >
             <hr className='mt-4'></hr >
-            <div className='d-flex dfadfad'>
+            <div className='d-flex dfadfad container mt-4 mb-4'>
                <h3 className='text-info'>Naim Biswas</h3>
                <h3 className='text-info'>CopyRight@2021 </h3>
             </div>
