6e678d881a78b600d908bb91427e3ae1c11509da
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-31 21:04:55 +0600

    create a card

--- src/PAGES/Home.js
+++ src/PAGES/Home.js
@@ -1,11 +1,28 @@
 import React, { Component } from 'react'
 
-export default class Home extends Component {
+export class Home extends Component {
    render() {
       return (
-         <div>
-            <h2>This is our blog home page</h2>
-         </div>
+         <div className='mt-4'>
+            <div className='card-gorup col-lg-4'>
+               <div className='card'>
+                  <img className='card-img-top' src='holder.js.100*180/' alt='Card Image'></img>
+                  <div className='card-body'>
+                     <h4 className='text-secondary mt-4'>
+                        This is card header
+                     </h4>
+                     <p className='text-secondary mt-2'>
+                        This is explame text This is explame text This is explame textThis is explame textThis is explame textThis is explame textThis is explame textThis is explame textThis is explame textThis is explame textThis is explame textThis is explame textThis is explame textThis is explame 
+                     </p>
+                     <button className='btn btn-info btn-md'>Read More</button>
+                  </div>
+               </div>
+            </div>
+            
+           
+          </div>
       )
    }
 }
+
+export default Home
