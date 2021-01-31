47fe3bc57085e9b3e5f3e597fb3629d4a6ae6e79
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-31 20:45:32 +0600

    add nav color margin padding

--- src/PAGES/Home.js
+++ src/PAGES/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
    render() {
       return (
          <div>
-            
+            <h2>This is our blog home page</h2>
          </div>
       )
    }
--- src/index.js
+++ src/index.js
@@ -1,30 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
+import  '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import reportWebVitals from './reportWebVitals';
 import { Route, Link, BrowserRouter as Router} from 'react-router-dom'
 import About from './components/About';
 import Portfolio from './components/Portfolio';
 import SingUpForm from './components/SingUpForm';
+import Home from './PAGES/Home';
 
 
 const MyRouter = (
-   <Router>
+   <div className='container pt-4 pb-4 text-white'>
+        <Router>
       <nav className="nav justify-content-center">
-        <li className="nav-link active"> <Link to='/'>Home</Link></li>
-        <li className="nav-link" ><Link to='/about'>About</Link></li>
-        <li className="nav-link" ><Link to='/portfolio'>Portfolio</Link></li>
-        <li className="nav-link" >  <Link to='/singup'>SingUp</Link></li>
+        <li className="nav-link  active"> <Link className='text-white bg-danger p-3' to='/'>Home</Link></li>
+        <li className="nav-link " ><Link className='text-white bg-danger p-3' to='/about'>About</Link></li>
+        <li className="nav-link " ><Link className='text-white bg-danger p-3' to='/portfolio'>Portfolio</Link></li>
+        <li className="nav-link " >  <Link className='text-white bg-danger p-3' to='/singup'>SingUp</Link></li>
       </nav>
        <div>
-         <Route exact path='/' component={App}></Route>
+          <Route exact path='/' component={Home}></Route>
          <Route path='/about' component={About}></Route>
          <Route path='/portfolio' component={Portfolio}></Route>
          <Route path='/singup' component={SingUpForm}></Route>
    </div>
+       
+   </Router> 
+   </div>
    
-   </Router>   
 )
 // ReactDOM.render(MyRouter, document.getElementById('root'))
 ReactDOM.render(
